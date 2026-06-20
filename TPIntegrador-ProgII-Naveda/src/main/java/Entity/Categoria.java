43162a2c78d9dfc3437f8afd63c2affda40f42cc
francosampieri <francosampiee@gmail.com>
2026-06-20 16:19:28 -0300

    Creo Categoria
    Creo el contenido de la clase Categoria

--- TPIntegrador-ProgII-Naveda/src/main/java/Entity/Categoria.java
+++ TPIntegrador-ProgII-Naveda/src/main/java/Entity/Categoria.java
@@ -1,4 +1,40 @@
 package Entity;
-public class Categoria {
 
+import java.time.LocalDateTime;
+
+public class Categoria extends Entidad {
+    private String nombre;
+    private String descripcion;
+    
+    //CONSTRUCTORES
+
+    public Categoria(long id, boolean eliminado, String nombre, String descripcion, LocalDateTime createdAt) {
+        super(id, eliminado, createdAt);
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Categoria(long id, String nombre, String descripcion) {
+        super(id);
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
+    //GETTERS SETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
