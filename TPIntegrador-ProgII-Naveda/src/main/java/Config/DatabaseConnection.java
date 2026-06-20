051bee247f8a8732eae3c0a520e39ca47941700d
francosampieri <francosampiee@gmail.com>
2026-06-20 19:34:42 -0300

    Creo DatabaseConnection
    Creo contenido de la clase DatabaseConnection

--- TPIntegrador-ProgII-Naveda/src/main/java/Config/DatabaseConnection.java
+++ TPIntegrador-ProgII-Naveda/src/main/java/Config/DatabaseConnection.java
@@ -1,4 +1,19 @@
 package Config;
-public class DatabaseConnection {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/food_store";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    //CONSTRUCTORES
+    
+    private DatabaseConnection() {}
+    
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD); 
+    }
 }
