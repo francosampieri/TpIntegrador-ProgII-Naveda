ebb89794588766ad7f363e71eef5f79d33ff907f
francosampieri <francosampiee@gmail.com>
2026-06-20 16:24:17 -0300

    Creo Producto
    Creo el contenido de la clase Producto

--- TPIntegrador-ProgII-Naveda/src/main/java/Entity/Producto.java
+++ TPIntegrador-ProgII-Naveda/src/main/java/Entity/Producto.java
@@ -1,4 +1,95 @@
 package Entity;
-public class Producto {
 
+import java.time.LocalDateTime;
+
+public class Producto extends Entidad {
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private int stock;
+    private String imagen;
+    private boolean disponible;
+    private Categoria categoria;
+    
+    //CONSTRUCTORES
+
+    public Producto(long id, boolean eliminado, LocalDateTime createdAt, String nombre, double precio, String descripcion, int stock, String imagen, boolean disponible, Categoria categoria) {
+        super(id, eliminado, createdAt);
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.disponible = disponible;
+        this.categoria = categoria;
+    }
+
+    public Producto(long id, String nombre, double precio, String descripcion, int stock, String imagen, boolean disponible, Categoria categoria) {
+        super(id);
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.disponible = disponible;
+        this.categoria = categoria;
+    }
+    
+    //GETTERS SETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
